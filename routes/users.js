f227a7721a12f0f2fdb91189e53c74fb66407e6b
Susheel Thapa <susheel.thapa@outlook.com>
2022-12-31 18:12:51 +0545

    Remove the password after decoding data from token, before sending decoded data to client

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 
+const _ = require("lodash");
 const express = require("express");
-const { createUser, validateUser } = require("../modules/User");
 const router = express.Router();
 
-const { getToken, getTokenData } = require("../services/token");
+const { createUser, validateUser } = require("../modules/User");
+const { createToken, getTokenData } = require("../services/token");
 
 /**
  * Route for creation of user and sending back the jsonwebtoken
@@ -16,9 +17,7 @@ router.post("/createuser", async (req, res) => {
 
   const { username, password, college, email, phone } = req.body;
 
-  const token = getToken({ username, password, college, email, phone });
-
-  const { success, error } = await createUser(
+  const { success, error, token } = await createUser(
     username,
     password,
     college,
@@ -41,29 +40,30 @@ router.post("/getuser", (req, res) => {
 
   const { token } = req.body;
 
-  const userDetails = getTokenData(token);
+  let userDetails = getTokenData(token);
+  userDetails = _.pick(userDetails, ["username", "college", "email", "phone"]);
 
   res.send(userDetails);
   res.end();
 });
 
-router.post("/login", async (req, res) => {
-  /**
-   * TODO: Validation of the data that is sent in request(i.e username, password)
-   */
-  const { username, password } = await req.body;
+// router.post("/login", async (req, res) => {
+//   /**
+//    * TODO: Validation of the data that is sent in request(i.e username, password)
+//    */
+//   const { username, password } = await req.body;
 
-  const { success, error } = await validateUser(username, password);
+//   const { success, error } = await validateUser(username, password);
 
-  const token = getToken({ username, password });
+//   const token = getToken({ username, password });
 
-  if (success) {
-    res.send({ success, token });
-  } else {
-    res.send({ success, error });
-  }
+//   if (success) {
+//     res.send({ success, token });
+//   } else {
+//     res.send({ success, error });
+//   }
 
-  res.end();
-});
+//   res.end();
+// });
 
 module.exports = router;
