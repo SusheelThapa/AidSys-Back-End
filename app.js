42f7fbc00b58b14f559b26bedad981fba04835e9
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-03 18:47:51 +0545

    Updated route handler
    Added login, signup and assets route handler

--- app.js
+++ app.js
@@ -15,8 +15,14 @@ app.use(cors());
 app.use(express.json());
 
 const users = require("./routes/users");
+const assets = require("./routes/assets");
+const login = require("./routes/login");
+const signup = require("./routes/signup");
 
-app.use("/api/user", users);
+app.use("/api/users", users);
+app.use("/api/assets", assets);
+app.use("/api/login", login);
+app.use("/api/signup", signup);
 
 app.listen(5000, () => {
   console.log("Listening on Port:5000");
