5361b0e2e271bc59f43f0e7e2b3c9fff09b168e4
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-04 00:52:40 +0545

    Create new route for tags (/api/tags)
    Only one route to get the list of tags has been implemented

--- app.js
+++ app.js
@@ -19,12 +19,14 @@ const assets = require("./routes/assets");
 const login = require("./routes/login");
 const signup = require("./routes/signup");
 const colleges = require("./routes/colleges");
+const tags = require("./routes/tags");
 
 app.use("/api/users", users);
 app.use("/api/assets", assets);
 app.use("/api/login", login);
 app.use("/api/signup", signup);
 app.use("/api/colleges", colleges);
+app.use("/api/tags", tags);
 
 app.listen(5000, () => {
   console.log("Listening on Port:5000");
