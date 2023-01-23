c178cea9efef2b6b98e1fc3cdef0a6ecffdf0a62
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-24 02:11:24 +0545

    Updated logic for getting studentID from jsonwebtoken

--- routes/token.js
+++ routes/token.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const express = require("express");
 const { createToken, getTokenData } = require("../services/token");
+const { Student } = require("../modules/Student");
 const router = express.Router();
 
 router.post("/create", async (req, res) => {
@@ -13,12 +14,16 @@ router.post("/create", async (req, res) => {
   res.send(token);
 });
 
-router.post("/data", (req, res) => {
+router.post("/data", async (req, res) => {
   const { token } = req.body;
 
   const data = getTokenData(token);
 
-  res.send(data);
+  const studentIsValid = await Student.find({ _id: data.studentID });
+
+  studentIsValid.length > 0
+    ? res.send(data)
+    : res.send({ error: true, message: "User doesn't exist" });
 });
 
 module.exports = router;
