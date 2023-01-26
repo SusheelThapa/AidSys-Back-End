8a0c04b2c2c4436177fc496032a197f3d0fcce46
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-26 15:44:57 +0545

    Updated Project collection

--- modules/Projects.js
+++ modules/Projects.js
@@ -3,22 +3,37 @@ const mongoose = require("mongoose");
 const projectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
+  owner: { type: mongoose.ObjectId, ref: "Student", required: true },
   link: { type: String, required: true },
-  tags: [{ type: String, required: true }],
-  member: [{ name: String, link: String }],
+  categories: [{ type: String, required: true }],
+  teammember: [{ name: String, link: String }],
 });
 
 const Project = mongoose.model("Project", projectSchema);
 
-const createProject = async (name, description, link, tags ,member) => {
+const createProject = async (
+  name,
+  description,
+  link,
+  owner,
+  categories,
+  teammember
+) => {
   try {
-    const newProject = new Project({ name, description, link, tags,member });
-
-    newProject.save().then((response) => {
-      console.log(`Project ${name} has been created with id ${response._id}`);
-
-      return { success: true, error: null, _id: newProject._id };
+    const project = new Project({
+      name,
+      description,
+      link,
+      owner,
+      categories,
+      teammember,
     });
+
+    project.save();
+
+    console.log(`Project ${name} has been created with id ${project._id}`);
+
+    return { success: true, error: null, _id: project._id };
   } catch (error) {
     console.log(error);
 
@@ -31,33 +46,11 @@ const createProject = async (name, description, link, tags ,member) => {
 };
 
 const getProjects = async () => {
-  try {
-    const projects = await Project.find({}, { __v: 0 });
-
-    return { success: true, error: null, projects };
-  } catch (error) {
-    console.log(error);
-    return {
-      success: null,
-      error: true,
-      message: "Error while  getting list of all the project",
-    };
-  }
+  return await Project.find({}, { __v: 0 });
 };
 
 const getProject = async (_id) => {
-  try {
-    const project = await Project.find({ _id }, { __v: 0 });
-
-    return { success: true, error: null, project };
-  } catch (error) {
-    console.log(error);
-    return {
-      success: null,
-      error: true,
-      message: "Error while  getting list of all the project",
-    };
-  }
+  return await Project.findOne({ _id }, { __v: 0 });
 };
 
-module.exports = { Project, createProject, getProjects, getProject };
+module.exports = { Project, createProject, getProject, getProjects };
