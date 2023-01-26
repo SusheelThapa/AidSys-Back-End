88e3b2dc4d34ef1e160262ce3c1972201353f230
Susheel Thapa <susheel.thapa@outlook.com>
2023-01-26 14:32:01 +0545

    Create a collection Auth
    It will store the login details of students

---
+++ modules/Auth.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const { encryptPassword, comparePassword } = require("../services/password");
+
+const authSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const Auth = mongoose.model("Auth", authSchema);
+
+const createNewAuth = async (username, password) => {
+  try {
+    const authExist = await Auth.find({ username });
+
+    if (authExist.length == 0) {
+      password = await encryptPassword(password);
+
+      let auth = new Auth({ username, password });
+
+      auth.save();
+
+      return auth._id;
+    }
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+};
+
+const validateAuth = async (username, password) => {
+  const auth = await Auth.findOne({ username: username });
+
+  if (auth) {
+    if (await comparePassword(password, auth.password)) {
+      return auth._id;
+    } else {
+      return undefined;
+    }
+  }
+
+  return undefined;
+};
